a9ff31ddede664388d7289df3a95f20dddf1ffe2
Akash Dewangan <akashdewangan55@gmail.com>
2025-07-18 00:11:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
+mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
